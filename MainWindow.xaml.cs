905c5b1475320ecc137394b57a7d72aafa34ece5
Fahri <68985644+fahriwps@users.noreply.github.com>
2024-04-29 09:59:41 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectWPFNET
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "😊", "😊",
+                "😀", "😀",
+                "😁", "😁",
+                "🤣", "🤣",
+                "😒", "😒",
+                "😘", "😘",
+                "😎", "😎",
+                "😢", "😢",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock texBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+
+            }
+        }
+    }
+}
